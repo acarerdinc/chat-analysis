a4951ef5c8366e3feb244205130c5bd561785907
Acar Erdinç <deusex@Acars-MBP.home>
2023-10-17 14:17:53 +0300

    setup.py and project structure modified

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chat-analysis",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    entry_points={
+        "console_scripts": [
+            "chat-analysis=chat_analysis.cli:main",
+        ],
+    },
+    install_requires=[
+        # List your dependencies here
+    ],
+)
